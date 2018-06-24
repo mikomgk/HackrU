03822818dbeb93bd04a007863914710b0a82fa2f
mikomgk <mikomgk@gmail.com>
2018-06-24 11:22:35 +0300

    lesson15

---
+++ JAVA/lesson15/src/com/company/MotionDetector.java
@@ -0,0 +1,31 @@
+package com.company;
+
+import java.awt.*;
+
+import static java.lang.System.currentTimeMillis;
+
+public class MotionDetector {
+    private OnDetectionListener listener;
+
+    public void setListener(OnDetectionListener listener) {
+        this.listener = listener;
+    }
+
+    public void runForEver() {
+        long z=currentTimeMillis();
+        while (true)
+            while ((currentTimeMillis()-z) % 500 == 0) {
+                PointerInfo a = MouseInfo.getPointerInfo();
+                Point b = a.getLocation();
+                int x = (int) b.getX();
+                int y = (int) b.getY();
+
+                if (listener != null)
+                    listener.onDetection(x,y);
+            }
+    }
+
+    public interface OnDetectionListener {
+        void onDetection(int x, int y);
+    }
+}
