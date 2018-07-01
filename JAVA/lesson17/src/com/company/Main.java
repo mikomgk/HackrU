17d7c22f07988aa558a6206f7dea5a2ed37af141
mikomgk <mikomgk@gmail.com>
2018-07-01 16:24:16 +0300

    lesson17

--- JAVA/lesson17/src/com/company/Main.java
+++ JAVA/lesson17/src/com/company/Main.java
@@ -1,19 +1,39 @@
 package com.company;
 
+import java.io.File;
 import java.util.HashSet;
 import java.util.Set;
 
 public class Main {
 
     public static void main(String[] args) {
-        Circle[] a = {new Circle(new Point(1,2),5),new Circle(new Point(5,3),3)};
-        Circle[] b = {new Circle(new Point(1,2),5),new Circle(new Point(1,2),5)};
+        System.out.println(countFiles("C:\\"));
+    }
+
+    private static int countFiles(String path) {
+        int count=0;
+        File file = new File(path);
+        if (file.isFile())
+            return 1;
+        else {
+            File[] files = file.listFiles();
+            if (files == null)
+                return 0;
+            for (int i = 0; i < files.length; i++) {
+                count += countFiles(files[i].getAbsolutePath());
+            }
+        }
+        return count;
+    }
+
+    private static void targil() {
+        Circle[] a = {new Circle(new Point(1, 2), 5), new Circle(new Point(5, 3), 3)};
+        Circle[] b = {new Circle(new Point(1, 2), 5), new Circle(new Point(1, 2), 5)};
         Set<Circle> mySet = new HashSet<>();
         for (int i = 0; i < a.length; i++) {
             mySet.add(a[i]);
         }
         System.out.println(containsArr(b, mySet));
-
     }
 
     private static boolean containsArr(Circle[] b, Set<Circle> mySet) {
@@ -40,10 +60,10 @@ public class Main {
         return r;
     }
 
-    private static void printArr(int[] a){
+    private static void printArr(int[] a) {
         System.out.print("{");
         for (int i = 0; i < a.length; i++) {
-            System.out.print(a[i]+",");
+            System.out.print(a[i] + ",");
         }
         System.out.print("}");
     }
