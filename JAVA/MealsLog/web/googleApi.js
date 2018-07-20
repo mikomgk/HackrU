21d2058036aff6159e295e34f31f14ba801d75a1
Miko Stern <mikomgk@gmail.com>
2018-07-20 14:16:31 +0300

    MealsLog

--- JAVA/MealsLog/web/googleApi.js
+++ JAVA/MealsLog/web/googleApi.js
@@ -1,10 +1,8 @@
 var fileID = '1KhFm1wksw_rMQzhcZir7bac1EioHOssSB_Kn6SPIZsI';
 var API_KEY = 'AIzaSyD9e-T-81qscSR9pW0olfucxhlWn1caU_g';
 var CLIENT_ID = '1018599316996-avst44ip93n283v5nti1i8u8ifk63vcj.apps.googleusercontent.com';
-var SCOPES = "https://www.googleapis.com/auth/spreadsheets.readonly";
+var SCOPES = "https://www.googleapis.com/auth/spreadsheets";
 var DISCOVERY_DOCS = ["https://sheets.googleapis.com/$discovery/rest?version=v4"];
-var authorizeButton = document.getElementById('authorize_button');
-var signoutButton = document.getElementById('signout_button');
 
 function handleClientLoad() {
     gapi.load('client:auth2', initClient);
@@ -19,10 +17,11 @@ function initClient() {
     }).then(function () {
         console.log(gapi.auth2.getAuthInstance().isSignedIn.get() ? "Logged In" : "Error");
         gapi.auth2.getAuthInstance().isSignedIn.listen(updateSignInStatus);
-        updateSignInStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
         gapi.auth2.getAuthInstance().signIn();
-    },function (reason) {
-        console.log(reason);
+        updateSignInStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+    },function (response) {
+        var errorMessage = response.result.error.message;
+        console.log(errorMessage);
     });
 }
 
@@ -41,7 +40,7 @@ function insertValues(_range, _values, _callback) {
         values: _values
     }).then(function (response) {
         var updatedCells = response.result.updates.updatedCells;
-        console.log("${updatedCells} cells updated.");
+        console.log(`${updatedCells} cells updated.`);
         _callback(updatedCells);
     }, function (response) {
         var errorMessage = response.result.error.message;
@@ -69,8 +68,8 @@ function readRange(_range, _callback) {
 function insertRow(_price, _description, _callback) {
     var d = new Date();
     var values = [
-        [_price, _description, d.getFullYear(), d.getMonth(), d.getDate()]
+        [_price, _description, d.getFullYear(), d.getMonth()+1, d.getDate()]
     ];
-    var range = 'Sheet1!B1:F1';
+    var range = 'Sheet1!A2:E2';
     insertValues(range, values, _callback);
 }
